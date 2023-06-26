926f0987b85982161f1c22f56f00c7df1f1b3f52
issam bourouail <issambourouail9@gmail.com>
2023-06-26 14:10:38 +0200

    add sound in app

--- main.js
+++ main.js
@@ -13,37 +13,75 @@ for (const key of keys) {
     audio.play()
     if (value == 'clear') {
       input = '';
-      display_input.value = input
+      display_input.innerHTML = input
       display_output.innerHTML = input
     } else if (value == 'backspace') {
       input = input.slice(0, -1)
-      display_input.value = input
+      display_input.innerHTML = clearInput(input)
     } else if (value == '=') {
       let result = eval(input)
-      display_output.innerHTML = result
+      display_output.innerHTML = result;
+      CleanOutput(result)
     } else if (value == 'brackets') {
       if (
         input.indexOf('(') == -1 ||
         input.indexOf('(') != -1 &&
         input.indexOf(')') != -1 &&
-        input.lastIndexOf('(') < input.lastIndexOf(')') 
+        input.lastIndexOf('(') < input.lastIndexOf(')')
       ) {
         input += "(";
-      }else if (
+      } else if (
         input.indexOf('(') != -1 &&
         input.indexOf(')') == -1 ||
         input.indexOf('(') != -1 &&
         input.indexOf(')') != -1 &&
-        input.lastIndexOf('(') > input.lastIndexOf(')') 
+        input.lastIndexOf('(') > input.lastIndexOf(')')
       ) {
         input += ")";
       }
 
-      display_input.value = input
+      display_input.innerHTML = clearInput(input)
     } else {
       input += value
-      display_input.value = input
+      display_input.innerHTML = clearInput(input)
     }
   })
-  
+
+}
+let clearInput = (input) => {
+  let input__array = input.split('')
+  for (let i = 0; i < input__array.length; i++) {
+    switch (input__array[i]) {
+      case '*':
+        input__array[i] = ' <span class="operator">x</span> '
+        break;
+      case '+':
+        input__array[i] = ' <span class="operator">+</span> '
+        break;
+      case '-':
+        input__array[i] = ' <span class="operator">-</span> '
+        break;
+      case '/':
+        input__array[i] = ' <span class="operator">÷</span> '
+        break;
+      case '%':
+        input__array[i] = `<span class="percent">%</span>`;
+        break;
+      case '(':
+        input__array[i] = `<span class="percent">(</span>`;
+        break;
+      case ')':
+        input__array[i] = `<span class="percent">)</span>`;
+        break;
+    }
+  }
+  return input__array.join('')
+}
+let CleanOutput = (output) => {
+  let output__string = output.toString();
+  let decimal = output__string.split('.')[1];
+  output__string = output__string.split('.')[0]
+  console.log(decimal)
+  console.log(output__string)
+
 }
